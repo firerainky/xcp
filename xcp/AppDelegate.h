95f71d291c46d52311018b02f052bee6652645ac
suchbalance <suchbalance@zhengkanyandeMacBook-Pro.local>
2016-04-13 23:11:06 +0800

    init

---
+++ xcp/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  xcp
+//
+//  Created by suchbalance on 16/4/13.
+//  Copyright © 2016年 suchbalance. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ xcp/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  xcp
+//
+//  Created by suchbalance on 16/4/13.
+//  Copyright © 2016年 suchbalance. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
